e841b910521f041e2aaf71465592f993569a87d7
sojeong2020 <macpark09@gmail.com>
2022-03-11 13:41:56 +0000

    alert

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,2 +1,20 @@
-<p>This is home</p>
-<p>alert coming!!</p>
+<script lang="ts">
+import { Alert } from 'sveltestrap';
+</script>
+
+<div class="alert">
+<Alert color="warning">
+    <h4 class="alert-heading text-capitalize">Covid 19 Community Tasks</h4>
+   <p> Looking for the covid specific community tasks?</p>
+    <a href="https://training10.teamkinetic.co.uk/vk/admin/" class="alert-link">
+        Open the app
+    </a>
+    
+  </Alert>
+</div>
+
+<style>
+    .alert {
+        text-align: center;
+    }
+</style>
