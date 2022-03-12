c224af5c82a35d2d671efa8a1a41203fde3d9b13
sojeong2020 <macpark09@gmail.com>
2022-03-12 15:24:35 +0000

    delete opp

--- src/components/OppDetail.svelte
+++ src/components/OppDetail.svelte
@@ -1,8 +1,19 @@
 <script>
+    import OppStore from '../store/oppStore.js';
+    import {Button} from 'sveltestrap';
+
     export let opp;
+
+    const handleDelete=(id) => {
+        OppStore.update(currentOpps=>{
+            return currentOpps.filter(opp=>opp.id !== id)
+        })
+    }
 </script>
 
 <div class="opp">
     <h4>{opp.name}</h4>
     <p>{opp.description}</p>
+
+    <Button on:click={()=>handleDelete(opp.id)}>Delete</Button>
 </div>
